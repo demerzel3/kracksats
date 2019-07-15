1848515dca674ad0c519c008905884706ca3d518
Gabriele Genta <gabriele.genta@gmail.com>
2019-07-15 22:45:20 +0200

    Add email notification for initiated withdrawal

--- lambdas/src/notifier.mjs
+++ lambdas/src/notifier.mjs
@@ -1,14 +1,20 @@
 import aws from 'aws-sdk';
 import KrakenClient from 'kraken-api';
 import {
+    allPass,
     applySpec,
-    prop,
-    propOr,
     compose,
+    cond,
+    pathEq,
+    prop,
+    propEq,
+    propOr,
+    T,
 } from 'ramda';
 
 import numberToString from './lib/numberToString';
 import readSecretJson from './lib/readSecretJson';
+import unwrapSnsEvent from './lib/unwrapSnsEvent';
 
 const CRYPTO_SYMBOL = 'XXBT';
 
@@ -32,42 +38,20 @@ const fetchWithdrawInfo = client =>
         limit: compose(parseFloat, propOr('0', 'limit')),
     }));
 
-exports.handler = (event, context) => {
-    const {
-        Records: [
-            {
-                Sns: {
-                    Message,
-                    MessageAttributes: {
-                        type: {
-                            Value: messageType,
-                        },
-                    },
-                },
-            },
-        ],
-    } = event;
+const sendOrderCompletedEmail = ({
+    order: {
+        vol_exec: amount,
+        price,
+    },
+}) => readSecretJson(KRAKEN_CREDENTIALS_ARN)
+    .then((credentials) => {
+        const client = new KrakenClient(credentials.API_KEY, credentials.API_SECRET);
 
-    if (messageType !== 'orderCompleted') {
-        return Promise.resolve(`Nothing to do here (messageType: ${messageType})`);
-    }
-
-    const {
-        order: {
-            vol_exec: amount,
-            price,
-        },
-    } = JSON.parse(Message);
-
-    return readSecretJson(KRAKEN_CREDENTIALS_ARN)
-        .then((credentials) => {
-            const client = new KrakenClient(credentials.API_KEY, credentials.API_SECRET);
-
-            return fetchWithdrawInfo(client);
-        })
-        .then(({ limit: totalAmount, fee: withdrawFee }) => {
-            const feePercent = ((withdrawFee / totalAmount) * 100).toFixed(2);
-            const body =
+        return fetchWithdrawInfo(client);
+    })
+    .then(({ limit: totalAmount, fee: withdrawFee }) => {
+        const feePercent = ((withdrawFee / totalAmount) * 100).toFixed(2);
+        const body =
 `Hi!
 
 I just bought ${amount} BTC @ €${price} on your behalf.
@@ -76,26 +60,70 @@ You have a total amount of ${numberToString(totalAmount)} BTC, you can withdraw 
 Respond with "Withdraw" to this message if you want to proceed.
 `;
 
-            const params = {
-                Destination: {
-                    ToAddresses: [RECIPIENT],
+        const params = {
+            Destination: {
+                ToAddresses: [RECIPIENT],
+            },
+            Message: {
+                Subject: {
+                    Charset: 'UTF-8',
+                    Data: 'Order completed',
                 },
-                Message: {
-                    Subject: {
+                Body: {
+                    Text: {
                         Charset: 'UTF-8',
-                        Data: 'Order completed',
-                    },
-                    Body: {
-                        Text: {
-                            Charset: 'UTF-8',
-                            Data: body,
-                        },
+                        Data: body,
                     },
                 },
-                Source: 'Kracksats <kracksats@demerzel3.dev>',
-            };
+            },
+            Source: 'Kracksats <kracksats@demerzel3.dev>',
+        };
 
-            return ses.sendEmail(params).promise();
-        })
-        .catch(e => console.error('Error sending notification mail', e));
+        return ses.sendEmail(params).promise();
+    });
+
+const sendWithdrawalInitiatedEmail = () => {
+    const body =
+`Hi!
+
+The withdrawal you have request has started, I will ping you when it hits the blockchain.
+`;
+
+    const params = {
+        Destination: {
+            ToAddresses: [RECIPIENT],
+        },
+        Message: {
+            Subject: {
+                Charset: 'UTF-8',
+                Data: 'Withdrawal initiated',
+            },
+            Body: {
+                Text: {
+                    Charset: 'UTF-8',
+                    Data: body,
+                },
+            },
+        },
+        Source: 'Kracksats <kracksats@demerzel3.dev>',
+    };
+
+    return ses.sendEmail(params).promise();
+};
+
+const isOrderCompleted = propEq('type', 'orderCompleted');
+const isWithdrawalInitiated = allPass([
+    propEq('type', 'withdrawalTransition'),
+    pathEq(['body', 'withdrawalStatus'], 'pendingExchange'),
+]);
+
+exports.handler = (event, context) => {
+    const snsEvent = unwrapSnsEvent(event);
+    const emailPromise = cond([
+        [isOrderCompleted, ({ body }) => sendOrderCompletedEmail(body)],
+        [isWithdrawalInitiated, ({ body }) => sendWithdrawalInitiatedEmail(body)],
+        [T, ({ type }) => Promise.resolve(`Nothing to do here (event type: ${type})`)],
+    ])(snsEvent)
+
+    return emailPromise.catch(e => console.error('Error sending notification mail', e));
 };
