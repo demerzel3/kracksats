18c309130c8ffbde54a67c23759b19d9c9ac5fc6
Gabriele Genta <gabriele.genta@gmail.com>
2019-07-18 21:31:19 +0200

    Store the whole message id complete with domain

--- lambdas/src/lib/storeEmail.mjs
+++ lambdas/src/lib/storeEmail.mjs
@@ -1,18 +1,33 @@
 import aws from 'aws-sdk';
 import {
+    complement,
     compose,
     either,
-    reject,
     isEmpty,
     isNil,
+    lensProp,
     map,
     objOf,
+    over,
     pick,
+    reject,
+    startsWith,
+    when,
 } from 'ramda';
+
+const {
+    AWS_REGION = 'us-west-1',
+} = process.env;
+
+const enrichAwsMessageId = when(
+    complement(startsWith('<')),
+    rawMessageId => `<${rawMessageId}@${AWS_REGION}.amazonses.com>`,
+);
 
 export default (tableName, emailDetails) => {
     const dynamodb = new aws.DynamoDB();
     const attributes = compose(
+        over(lensProp('messageId'), enrichAwsMessageId),
         map(objOf('S')),
         reject(either(isNil, isEmpty)),
         pick([
